6b60380a213586ba5cfc894c96bdcfe107311730
primaftr <primafitra123@gmail.com>
2024-06-16 21:01:06 +0700

    chore: enable cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: [process.env.DOMAIN_URL],
+  });
+
   await app.listen(3000);
 }
 bootstrap();
