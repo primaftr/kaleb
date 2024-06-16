0272c9d490c77e240d134cf1ff12c577aa0342dd
primaftr <primafitra123@gmail.com>
2024-06-16 21:04:46 +0700

    feat: add crud users func

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,4 +1,38 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma.service';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async findOne(email: string) {
+    return this.prisma.user.findUnique({ where: { email } });
+  }
+
+  async create(user: { email: string; password: string; name: string }) {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(user.password, salt);
+    return this.prisma.user.create({
+      data: {
+        email: user.email,
+        password: hashedPassword,
+        name: user.name,
+      },
+    });
+  }
+
+  async save(user: any) {
+    return this.prisma.user.update({
+      where: { email: user.email },
+      data: user,
+    });
+  }
+
+  async update(user: any) {
+    return this.prisma.user.update({
+      where: { email: user.email },
+      data: user,
+    });
+  }
+}
