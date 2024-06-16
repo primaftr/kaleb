6af11939d75f29713a48722eb7ba9914c3bbc835
primaftr <primafitra123@gmail.com>
2024-06-17 00:51:54 +0700

    chore: update to personal eslint

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -1,25 +1,62 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: 'tsconfig.json',
-    tsconfigRootDir: __dirname,
-    sourceType: 'module',
+  "root": true,
+  "ignorePatterns": ["**/*", "node_modules/**"],
+  "extends": ["prettier"],
+  "plugins": ["@nx", "simple-import-sort", "prettier"],
+  "rules": {
+    "prettier/prettier": "error",
+    "arrow-body-style": "off",
+    "prefer-arrow-callback": "off"
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ],
-  root: true,
-  env: {
-    node: true,
-    jest: true,
-  },
-  ignorePatterns: ['.eslintrc.js'],
-  rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-  },
-};
+  "overrides": [
+    {
+      "files": ["*.ts", "*.tsx", "*.js", "*.jsx"],
+      "rules": {
+        "@nx/enforce-module-boundaries": [
+          "error",
+          {
+            "enforceBuildableLibDependency": true,
+            "allow": [],
+            "depConstraints": [
+              {
+                "sourceTag": "*",
+                "onlyDependOnLibsWithTags": ["*"]
+              }
+            ]
+          }
+        ],
+        "simple-import-sort/imports": "error",
+        "@typescript-eslint/no-unused-vars": [
+          "error",
+          {
+            "argsIgnorePattern": "^_"
+          }
+        ]
+      }
+    },
+    {
+      "files": ["*.ts", "*.tsx"],
+      "extends": ["plugin:@nx/typescript", "plugin:security/recommended"],
+      "rules": {
+        "@typescript-eslint/no-explicit-any": ["off"],
+        "security/detect-object-injection": ["off"],
+        "security/detect-non-literal-fs-filename": ["off"],
+        "simple-import-sort/imports": "error",
+        "object-shorthand": ["error", "always"],
+        "no-useless-rename": ["error"]
+      }
+    },
+    {
+      "files": ["*.js", "*.jsx"],
+      "extends": ["plugin:@nx/javascript", "plugin:security/recommended"],
+      "rules": {
+        "simple-import-sort/imports": "error"
+      }
+    },
+    {
+      "files": "*.json",
+      "parser": "jsonc-eslint-parser",
+      "rules": {}
+    }
+  ]
+}
--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -1,15 +1,25 @@
 /* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
+require('@rushstack/eslint-patch/modern-module-resolution');
 
 module.exports = {
   root: true,
-  'extends': [
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier/skip-formatting'
+    '@vue/eslint-config-prettier'
+  ],
+  overrides: [
+    {
+      files: ['**/__tests__/*.{cy,spec}.{js,ts,jsx,tsx}'],
+    },
   ],
   parserOptions: {
-    ecmaVersion: 'latest'
-  }
-}
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    'no-unused-vars': 'off',
+    'no-undef': 'off',
+    'vue/multi-word-component-names': 'off',
+  },
+};
